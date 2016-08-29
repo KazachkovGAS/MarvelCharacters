d8091b2133e79d87b6562fe8f09a4fa7cfa2708d
KazachkovGAS <kazachkovgas@gmail.com>
2016-08-29 20:32:10 +0400

    Add files via upload

---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Marvel Characters
+//
+//  Created by Максим on 29.08.16.
+//  Copyright © 2016 Максим. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Marvel Characters
+//
+//  Created by Максим on 29.08.16.
+//  Copyright © 2016 Максим. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
